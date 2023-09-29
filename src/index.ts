fb61fb571f2391291bf3414efdb6f6156214f2ac
Cano <developer.sasazono@gmail.com>
2023-09-29 10:10:51 +0900

    add target, template option

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,22 @@
 import { getInput } from "@actions/core";
-import { readFile, writeFile } from "fs/promises";
+import { copyFile, readFile, writeFile } from "fs/promises";
 import { resolve } from "path";
 import { getProcessor } from "processor";
 
 const token = getInput("token", { required: true });
 const userName = getInput("user-name", { required: true });
 
-const targetPath = resolve("./README.md");
+const optTarget = getInput("target", { required: true });
+const targetPath = resolve(optTarget);
+
+const optTemplate = getInput("template");
+const templatePath = resolve(optTemplate);
 
 const main = async () => {
+  if (templatePath) {
+    await copyFile(templatePath, targetPath);
+  }
+
   let md = await readFile(targetPath, { encoding: "utf-8" });
 
   const vFile = await getProcessor(token, userName).process(md);
