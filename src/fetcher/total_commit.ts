85a462740be8b912518a2ce5768397da165ce55c
Cano <developer.sasazono@gmail.com>
2023-12-10 15:22:37 +0900

    update

--- src/fetcher/total_commit.ts
+++ src/fetcher/total_commit.ts
@@ -1,65 +1,17 @@
-import { getOctokit } from "@actions/github";
-import { User } from "@octokit/graphql-schema";
-
-const query = /* GraphQL */ `
-  query ($userName: String!, $cursor: String) {
-    user(login: $userName) {
-      repositories(
-        ownerAffiliations: OWNER
-        isFork: false
-        orderBy: { field: STARGAZERS, direction: DESC }
-        first: 100
-        after: $cursor
-      ) {
-        pageInfo {
-          hasNextPage
-          endCursor
-        }
-        nodes {
-          defaultBranchRef {
-            target {
-              ... on Commit {
-                __typename
-                history {
-                  totalCount
-                }
-              }
-            }
-          }
-        }
-      }
-    }
-  }
-`;
-
 export const fetchTotalCommit = async (
   token: string,
   userName: string
 ): Promise<number> => {
-  const octokit = getOctokit(token);
+  const url = new URL("https://api.github.com/search/commits");
+  url.searchParams.append("q", `author:${userName}`);
 
-  let count = 0;
+  const res = await fetch(url, {
+    headers: {
+      Authorization: `token ${token}`,
+    },
+  });
 
-  let hasNextPage = true;
-  let cursor: string | null = null;
-  do {
-    const params: { [param: string]: unknown } = { userName, cursor };
-    const response = await octokit.graphql<{ user: User }>(query, params);
+  const data = await res.json();
 
-    const repo = response.user.repositories;
-
-    count +=
-      repo.nodes?.reduce((p, c) => {
-        const target = c?.defaultBranchRef?.target;
-
-        return target?.__typename !== "Commit"
-          ? p
-          : p + (target.history.totalCount || 0);
-      }, 0) ?? 0;
-
-    hasNextPage = repo.pageInfo.hasNextPage;
-    cursor = repo.pageInfo.endCursor || null;
-  } while (hasNextPage);
-
-  return count;
+  return data.total_count;
 };
